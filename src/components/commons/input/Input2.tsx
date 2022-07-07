f17b8e11cd5f92abca5695f9e9c1909a8afc15ee
yeontan0826 <yeontan0826@gmail.com>
2022-07-07 15:04:47 +0900

    [#23] Feat : Input 컴포넌트 생성 완료

--- src/components/commons/input/Input2.tsx
+++ src/components/commons/input/Input2.tsx
@@ -1,32 +1,35 @@
 /*
-* Parameter
-* placeholder(string), onChangeText(method)
-*/
+ * Parameter
+ * placeholder(string), onChangeText(method)
+ */
 
-import styled from "@emotion/native"
+import styled from "@emotion/native";
 import { useState } from "react";
 import { NativeSyntheticEvent, TextInputChangeEventData } from "react-native";
 
-interface IInput1Props {
-    placeholder?: string
-    onChangeText?: (e: NativeSyntheticEvent<TextInputChangeEventData>) => void;
+interface IInput2Props {
+   placeholder?: string;
+   onChangeText?: (e: NativeSyntheticEvent<TextInputChangeEventData>) => void;
 }
 
-export default function Input1(props: IInput1Props) {
-    const [isFocus, setIsFocus] = useState(false);
+export default function Input2(props: IInput2Props) {
+   const [isFocus, setIsFocus] = useState(false);
 
-    return <CustomTextInput
-                onFocus={() => setIsFocus(true)}
-                onBlur={() => setIsFocus(false)}
-                style={{borderBottomColor: isFocus ? "#5D8BFF" : "black"}}
-                placeholderTextColor={"#A5A5A5"}
-                placeholder={props.placeholder} 
-                onChange={props.onChangeText}/>
+   return (
+      <CustomTextInput
+         onFocus={() => setIsFocus(true)}
+         onBlur={() => setIsFocus(false)}
+         style={{ borderBottomColor: isFocus ? "#5D8BFF" : "black" }}
+         placeholderTextColor={"#A5A5A5"}
+         placeholder={props.placeholder}
+         onChange={props.onChangeText}
+      />
+   );
 }
 
 const CustomTextInput = styled.TextInput`
-    width: 100%;
-    height: 45px;
-    padding: 0 8px;
-    border-bottom-width: 1px;
-`
+   width: 100%;
+   height: 45px;
+   padding: 0 8px;
+   border-bottom-width: 1px;
+`;
