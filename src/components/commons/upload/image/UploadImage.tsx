415cd3331765f5ab10e2a8675c82a3028bbeb879
yeontan0826 <yeontan0826@gmail.com>
2022-07-09 15:51:12 +0900

    [#55] Modify : Image Upload 컴포넌트 로직 수정

--- src/components/commons/upload/image/UploadImage.tsx
+++ src/components/commons/upload/image/UploadImage.tsx
@@ -1,16 +1,41 @@
 import * as R from "react-native";
 import * as S from "./UploadImage.styles";
 import { IUploadImageProps } from "./UploadImage.types";
+import * as ImagePicker from "expo-image-picker";
 
 export default function UploadImage(props: IUploadImageProps) {
+   // 권한 요청을 위한 hooks
+   const [status, requestPermission] = ImagePicker.useMediaLibraryPermissions();
+
+   const uploadImage = async () => {
+      // 퍼미션 승인 여부
+      if (!status?.granted) {
+         const permission = await requestPermission();
+         if (!permission.granted) return;
+      }
+
+      const result = await ImagePicker.launchImageLibraryAsync({
+         mediaTypes: ImagePicker.MediaTypeOptions.Images,
+         allowsEditing: true,
+         quality: 1,
+         aspect: [1, 1],
+      });
+
+      if (result.cancelled) return;
+
+      const newImageFiles = [...props.imageFiles];
+      newImageFiles[props.index] = result.uri;
+      props.setImageFiles(newImageFiles);
+   };
+
    return (
       <S.Wrapper>
          {props.imageFile !== "" ? (
-            <R.Pressable onPress={() => props.uploadImage(props.index)}>
+            <R.Pressable onPress={uploadImage}>
                <S.CarImage source={{ uri: props.imageFile }} />
             </R.Pressable>
          ) : (
-            <S.Box onPress={() => props.uploadImage(props.index)}>
+            <S.Box onPress={uploadImage}>
                <S.RowLine />
                <S.ColumnLine />
             </S.Box>
