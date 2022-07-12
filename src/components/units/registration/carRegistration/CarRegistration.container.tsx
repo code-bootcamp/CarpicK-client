a811ce81dd23d63cee0ec814091a53e6cb166c21
yeontan0826 <yeontan0826@gmail.com>
2022-07-12 13:33:54 +0900

    [#124] Feat : RN 이미지 프로세스 초기 세팅

--- src/components/units/registration/carRegistration/CarRegistration.container.tsx
+++ src/components/units/registration/carRegistration/CarRegistration.container.tsx
@@ -1,11 +1,41 @@
+import { useMutation } from "@apollo/client";
+import { ReactNativeFile } from "apollo-upload-client";
 import { useState } from "react";
 import CarRegistrationUI from "./CarRegistration.presenter";
+import { UPLOAD_FILE } from "./CarRegistration.queries";
+
+const generateRNFile = (uri, name) => {
+   return uri
+      ? new ReactNativeFile({
+           uri,
+           type: "image",
+           name,
+        })
+      : null;
+};
 
 export default function CarRegistrationPage({ navigation }) {
-   const [imageFiles, setImageFiles] = useState<string[]>([""]);
+   const [uploadFile] = useMutation(UPLOAD_FILE);
 
-   const onPressRegister = () => {
+   const [isModalVisible, setIsModalVisible] = useState(false);
+   const [imageFiles, setImageFiles] = useState([""]);
+
+   const onPressRegister = async () => {
       // TODO 마이카 api 요청 후 메인페이지로 이동
+      console.log(imageFiles);
+      const file = generateRNFile(imageFiles[0], `picture-${Date.now()}`);
+      try {
+         const imageUrl = await uploadFile({
+            variables: {
+               files: file,
+            },
+         });
+
+         console.log(imageUrl);
+      } catch (error: any) {
+         console.log("실패!!");
+         console.error(error.message);
+      }
    };
 
    return (
@@ -13,6 +43,8 @@ export default function CarRegistrationPage({ navigation }) {
          imageFiles={imageFiles}
          setImageFiles={setImageFiles}
          onPressRegister={onPressRegister}
+         isModalVisible={isModalVisible}
+         setIsModalVisible={setIsModalVisible}
       />
    );
 }
