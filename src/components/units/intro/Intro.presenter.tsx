c8cde003e0020a4e7fe374598abf3ba681e97d6d
MH-yeo <mh.yeo27@gmail.com>
2022-07-25 12:08:10 +0900

    [#261] Modify : Intro 3 image 수정 , customer service 이미지 수정

--- src/components/units/intro/Intro.presenter.tsx
+++ src/components/units/intro/Intro.presenter.tsx
@@ -6,8 +6,25 @@ import Intro1 from "../../../../assets/intro/Intro1-r1.svg";
 import Intro2 from "../../../../assets/intro/Intro2-r1.svg";
 import Intro3 from "../../../../assets/intro/Intro3-r1.svg";
 import colors from "../../../commons/lib/colors";
+import TitleText from "../../commons/text/TitleText";
 
 export const SLIDER_WIDTH = R.Dimensions.get("window").width;
+
+const Intro3JSX = () => {
+   return (
+      <>
+         <R.View style={{ alignItems: "center" }}>
+            <TitleText fontSize="21" color={colors.theme}>
+               원하는 시간과 장소에서
+            </TitleText>
+            <TitleText fontSize="21">예약과 반납을 스마트폰 하나로!</TitleText>
+            <R.View style={{ marginTop: 45 }}>
+               <Intro3 />
+            </R.View>
+         </R.View>
+      </>
+   );
+};
 
 const data = [
    {
@@ -23,7 +40,7 @@ const data = [
    {
       id: 3,
       name: "Intro3",
-      image: Intro3,
+      image: Intro3JSX,
    },
 ];
 
